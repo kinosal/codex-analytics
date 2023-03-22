569dae89212653da9e42cd77981658f154f3f2bc
Nikolas Schriefer <info@nikolasschriefer.de>
2023-03-22 11:58:41 +0100

    Update to GPT3.5 Chat due to Codex deprecation

--- README.md
+++ README.md
@@ -8,27 +8,20 @@ https://share.streamlit.io/kinosal/codex-analytics/app.py
 This [Streamlit](https://streamlit.io) mini-app translates a statement in plain
 English into [SQL](https://en.wikipedia.org/wiki/SQL) or
 [Python](https://www.python.org) [Pandas](https://pandas.pydata.org)
-using OpenAI's GPT-3 based [Codex](https://openai.com/blog/openai-codex/) model,
+using OpenAI's [GPT-3.5](https://platform.openai.com/docs/models/gpt-3-5) Turbo (Chat) model,
 allowing anyone not familiar with those languages/libraries to perform simple data
 analysis tasks with SQL databases or Pandas DataFrames.
 
 The app generates a text prompt describing the table or DataFrame and
 adding a comment with the provided query statement from a few user inputs. The
-prompt is then sent to the OpenAI API which - using the GPT-3 davinci-codex
-engine that has been trained on a lot of publicly available text content
-including open-source code - predicts the next likely to be used tokens
-and thus generates the translated statement.
-
-This is not perfect and does occasionally result in false statements with
-a wrong syntax (less frequent as far as I could observe)
-and/or unexpected behavior (more frequent). In my experiments the
-translation to SQL also yielded better results than Pandas.
+prompt is then sent to the OpenAI API which returns the next likely to be used tokens
+for a possible answer and thus generates the translated statement.
 
 ## Contribution
 
 I hope this will be of value to people learning, wanting or needing
 to use SQL and/or Pandas for data analytics as well as for people trying
-to understand GPT-3's and specifically Codex's capabilities in this field.
+to understand the capabilities of GPT models in this field.
 
 Please reach out to me at nikolas@schriefer.me with any feedback -
 especially suggestions to improve this - or questions you might have.
--- app.py
+++ app.py
@@ -19,21 +19,16 @@ def openai_call(prompt: str, stop: str = None) -> str:
         stop: stop sequence to interrupt further token generation
     Return: predicted response text
     """
+    print(prompt)
     kwargs = {
-        "engine": "code-davinci-002",
-        "prompt": prompt,
-        "max_tokens": 64,
-        "temperature": 0,
-        "top_p": 1,
-        "frequency_penalty": 0,
-        "presence_penalty": 0,
-        "best_of": 1
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": prompt}]
     }
     if stop:
         kwargs["stop"] = stop
-    response = openai.Completion.create(**kwargs)
-    # print(response)
-    return response["choices"][0]["text"]
+    response = openai.ChatCompletion.create(**kwargs)
+    print(response)
+    return response["choices"][0]["message"]["content"]
 
 
 ####
@@ -50,7 +45,7 @@ st.write(
     """This mini-app translates a statement in plain English into
     [SQL](https://en.wikipedia.org/wiki/SQL) or
     [Python](https://www.python.org) [Pandas](https://pandas.pydata.org)
-    using OpenAI's GPT-3 based Codex model, allowing anyone
+    using OpenAI's GPT-3.5 model, allowing anyone
     not familiar with those languages/libraries to perform simple data
     analysis tasks with SQL databases or Pandas DataFrames."""
 )
@@ -58,24 +53,16 @@ st.write(
 st.write(
     """The following inputs will generate a text prompt describing the table or
     DataFrame and adding a comment with the provided query statement. The
-    prompt is then sent to the OpenAI API which - using the GPT-3 davinci-codex
-    engine that has been trained on a lot of publicly available text content
-    including open-source code - predicts the next likely to be used tokens
-    and thus generates the translated statement."""
-)
-
-st.write(
-    """As you will see this is not perfect and does occasionally result in false
-    statements with a wrong syntax (less frequent as far as I could observe)
-    and/or unexpected behavior (more frequent). In my experiments the
-    translation to SQL also yielded better results than Pandas.
-    """
+    prompt is then sent to the OpenAI API which - using the GPT-3.5 Turbo (Chat)
+    model that has been trained on a lot of publicly available text content
+    including open-source code - predicts and returns the likely to be used tokens for a
+    possible answer and thus generates the translated statement."""
 )
 
 st.write(
     """I hope this will be of value to people learning, wanting or needing
     to use SQL and/or Pandas for data analytics as well as for people trying
-    to understand GPT-3's and specifically Codex's capabilities in this field.
+    to understand the capabilities of GPT models in this field.
     Please reach out to me at nikolas@schriefer.me with any feedback -
     especially suggestions to improve - or questions you might
     have. You can find the code for this app on
@@ -131,7 +118,7 @@ if statement:
             # {statement}
             {table_name}.'''
         )
-        stop = "\n"
+        stop = None
         result_prefix = table_name + "."
         language = "python"
 
